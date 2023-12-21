fd98a65f59e65ad99cdf1a42360ef4f6693eccea
hazelkimberly <hazelkimc@gmail.com>
2023-12-21 04:10:01 -0800

    Pseudocode for scheduleRoutes.js

---
+++ server/scheduleRoutes.js
@@ -0,0 +1,12 @@
+// Import express
+// Create new router object
+
+// Get schedule
+
+// Add an event to schedule
+
+// Edit an event on schedule
+
+// Delete an event on schedule
+
+// Export router object
